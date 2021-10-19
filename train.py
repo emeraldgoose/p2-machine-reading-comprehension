3daad225e3a26e006f192cd8453972511dde35a8
emeraldgoose <emeraldgoose@gmail.com>
2021-10-19 05:24:06 +0000

    python train.py가 가능함

---
+++ train.py
@@ -0,0 +1,232 @@
+import logging
+import os
+import sys
+
+from typing import List, Callable, NoReturn, NewType, Any
+import dataclasses
+from datasets import load_metric, load_from_disk, Dataset, DatasetDict
+
+from transformers import AutoConfig, AutoModelForQuestionAnswering, AutoTokenizer
+
+from transformers import (
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    TrainingArguments,
+    set_seed,
+)
+
+from tokenizers import Tokenizer
+from tokenizers.models import WordPiece
+
+from utils_qa import postprocess_qa_predictions, check_no_error
+from trainer_qa import QuestionAnsweringTrainer
+from retrieval import SparseRetrieval
+
+from arguments import (
+    ModelArguments,
+    DataTrainingArguments,
+)
+
+from preprocessing import eval_preprocessor, train_preprocessor
+from postprocessing import postprocessor
+
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> NoReturn:
+    # 가능한 arguments 들은 ./arguments.py 나 transformer package 안의 src/transformers/training_args.py 에서 확인 가능합니다.
+    # --help flag 를 실행시켜서 확인할 수 도 있습니다.
+
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses(
+        args_filename="do_train_arguments.txt"
+    )
+
+    # set training arguments
+    training_args.per_device_train_batch_size = 8
+    training_args.learning_rate = 5e-5
+    training_args.num_train_epochs = 1
+    training_args.weight_decay = 0.01
+    training_args.logging_steps = 100
+    training_args.save_total_limit = 5
+
+    print(f"model is from {model_args.model_name_or_path}")
+    print(f"data is from {data_args.dataset_name}")
+    print(f"models saved at {training_args.output_dir}")
+    print(f"overwrite_output_dir : {training_args.overwrite_output_dir}")
+    print(f"do_train : {training_args.do_train}")
+
+    # logging 설정
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # 모델을 초기화하기 전에 난수를 고정합니다.
+    set_seed(42)
+
+    datasets = load_from_disk(data_args.dataset_name)
+    print(datasets)
+
+    # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
+    # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
+    config = AutoConfig.from_pretrained(
+        model_args.config_name
+        if model_args.config_name is not None
+        else model_args.model_name_or_path,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name
+        if model_args.tokenizer_name is not None
+        else model_args.model_name_or_path,
+        # 'use_fast' argument를 True로 설정할 경우 rust로 구현된 tokenizer를 사용할 수 있습니다.
+        # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며,
+        # rust version이 비교적 속도가 빠릅니다.
+        use_fast=True,
+    )
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+    )
+
+    # dataset을 전처리합니다.
+    # training과 evaluation에서 사용되는 전처리는 아주 조금 다른 형태를 가집니다.
+    if training_args.do_train:
+        column_names = datasets["train"].column_names
+    else:
+        column_names = datasets["validation"].column_names
+
+    question_column_name = "question" if "question" in column_names else column_names[0]
+    context_column_name = "context" if "context" in column_names else column_names[1]
+    answer_column_name = "answers" if "answers" in column_names else column_names[2]
+
+    # Padding에 대한 옵션을 설정합니다.
+    # (question|context) 혹은 (context|question)로 세팅 가능합니다.
+    pad_on_right = tokenizer.padding_side == "right"
+
+    # 오류가 있는지 확인합니다.
+    last_checkpoint, max_seq_length = check_no_error(
+        data_args, training_args, datasets, tokenizer
+    )
+
+    if training_args.do_train:
+        if "train" not in datasets:
+            raise ValueError("--do_trai장n requires a train dataset")
+        train_dataset = datasets["train"]
+
+        # dataset에서 train feature를 생성합니다.
+        train_dataset = train_dataset.map(
+            train_preprocessor(
+                tokenizer,
+                data_args,
+                max_seq_length,
+                pad_on_right,
+                question_column_name,
+                context_column_name,
+                answer_column_name,
+            ),
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    if training_args.do_eval:
+        eval_dataset = datasets["validation"]
+
+        # Validation Feature 생성
+        eval_dataset = eval_dataset.map(
+            eval_preprocessor(
+                tokenizer,
+                data_args,
+                max_seq_length,
+                pad_on_right,
+                question_column_name,
+                context_column_name,
+                answer_column_name,
+            ),
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    # Data collator
+    # flag가 True이면 이미 max length로 padding된 상태입니다.
+    # 그렇지 않다면 data collator에서 padding을 진행해야합니다.
+    data_collator = DataCollatorWithPadding(
+        tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
+    )
+
+    metric = load_metric("squad")
+
+    def compute_metrics(p: EvalPrediction):
+        return metric.compute(predictions=p.predictions, references=p.label_ids)
+
+    # Trainer 초기화
+    trainer = QuestionAnsweringTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        eval_examples=datasets["validation"] if training_args.do_eval else None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=postprocessor(data_args, datasets, answer_column_name),
+        compute_metrics=compute_metrics,
+    )
+
+    # Training
+    if training_args.do_train:
+        if last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        elif os.path.isdir(model_args.model_name_or_path):
+            checkpoint = model_args.model_name_or_path
+        else:
+            checkpoint = None
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(train_dataset)
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        output_train_file = os.path.join(training_args.output_dir, "train_results.txt")
+
+        with open(output_train_file, "w") as writer:
+            logger.info("***** Train results *****")
+            for key, value in sorted(train_result.metrics.items()):
+                logger.info(f"  {key} = {value}")
+                writer.write(f"{key} = {value}\n")
+
+        # State 저장
+        trainer.state.save_to_json(
+            os.path.join(training_args.output_dir, "trainer_state.json")
+        )
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+
+        metrics["eval_samples"] = len(eval_dataset)
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+
+if __name__ == "__main__":
+
+    main()
