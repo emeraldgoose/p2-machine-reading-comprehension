b82dab5581689f4324d5f4d453cbbd382828d779
choesuhong <csoohong64@gmail.com>
2021-11-02 14:45:24 +0900

    minseong branch

--- arguments.py
+++ arguments.py
@@ -9,7 +9,7 @@ class ModelArguments:
     """
 
     model_name_or_path: str = field(
-        default="klue/bert-base",
+        default="klue/roberta-large",
         metadata={
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
         },
@@ -26,6 +26,12 @@ class ModelArguments:
             "help": "Pretrained tokenizer name or path if not the same as model_name"
         },
     )
+    dropout: float = field(
+        default=0.1, metadata={"help": "lstmOnRoberta's LSTM layer dropout probability"}
+    )
+    ver: str = field(
+        default="original",
+    )
 
 
 @dataclass
@@ -39,7 +45,7 @@ class DataTrainingArguments:
         metadata={"help": "The name of the dataset to use."},
     )
     overwrite_cache: bool = field(
-        default=False,
+        default=True,
         metadata={"help": "Overwrite the cached training and evaluation sets"},
     )
     preprocessing_num_workers: Optional[int] = field(
@@ -47,24 +53,18 @@ class DataTrainingArguments:
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
     max_seq_length: int = field(
-        default=384,
+        default=512,
         metadata={
             "help": "The maximum total input sequence length after tokenization. Sequences longer "
             "than this will be truncated, sequences shorter will be padded."
         },
     )
     pad_to_max_length: bool = field(
-        default=False,
+        default=True,
         metadata={
             "help": "Whether to pad all samples to `max_seq_length`. "
             "If False, will pad the samples dynamically when batching to the maximum length in the batch (which can "
             "be faster on GPU but will be slower on TPU)."
-        },
-    )
-    doc_stride: int = field(
-        default=128,
-        metadata={
-            "help": "When splitting up a long document into chunks, how much stride to take between chunks."
         },
     )
     max_answer_length: int = field(
@@ -74,12 +74,19 @@ class DataTrainingArguments:
             "and end predictions are not conditioned on one another."
         },
     )
+    # 아래부터 retriever 영역
+    doc_stride: int = field(
+        default=128,
+        metadata={
+            "help": "When splitting up a long document into chunks, how much stride to take between chunks."
+        },
+    )
     eval_retrieval: bool = field(
         default=True,
         metadata={"help": "Whether to run passage retrieval using sparse embedding."},
     )
     num_clusters: int = field(
-        default=64, metadata={"help": "Define how many clusters to use for faiss."}
+        default=128, metadata={"help": "Define how many clusters to use for faiss."}
     )
     top_k_retrieval: int = field(
         default=1,
@@ -88,5 +95,5 @@ class DataTrainingArguments:
         },
     )
     use_faiss: bool = field(
-        default=False, metadata={"help": "Whether to build with faiss"}
+        default=True, metadata={"help": "Whether to build with faiss"}
     )
---
+++ make_dataset.py
@@ -0,0 +1,72 @@
+from preprocessing import eval_preprocessor, train_preprocessor
+
+
+def make_dataset(data_args, datasets, tokenizer, max_seq_length):
+
+    train_column_names = datasets["train"].column_names
+
+    train_question_column_name = (
+        "question" if "question" in train_column_names else train_column_names[0]
+    )
+    train_context_column_name = (
+        "context" if "context" in train_column_names else train_column_names[1]
+    )
+    train_answer_column_name = (
+        "answers" if "answers" in train_column_names else train_column_names[2]
+    )
+
+    # Padding에 대한 옵션을 설정합니다.
+    # (question|context) 혹은 (context|question)로 세팅 가능합니다.
+    pad_on_right = tokenizer.padding_side == "right"
+
+    train_dataset = datasets["train"]
+
+    # dataset에서 train feature를 생성합니다.
+    train_dataset = train_dataset.map(
+        train_preprocessor(
+            tokenizer,
+            data_args,
+            max_seq_length,
+            pad_on_right,
+            train_question_column_name,
+            train_context_column_name,
+            train_answer_column_name,
+        ),
+        batched=True,
+        num_proc=data_args.preprocessing_num_workers,
+        remove_columns=train_column_names,
+        load_from_cache_file=not data_args.overwrite_cache,
+    )
+
+    eval_column_names = datasets["validation"].column_names
+
+    eval_question_column_name = (
+        "question" if "question" in eval_column_names else eval_column_names[0]
+    )
+    eval_context_column_name = (
+        "context" if "context" in eval_column_names else eval_column_names[1]
+    )
+    eval_answer_column_name = (
+        "answers" if "answers" in eval_column_names else eval_column_names[2]
+    )
+
+    eval_dataset = datasets["validation"]
+
+    # Validation Feature 생성
+    eval_dataset = eval_dataset.map(
+        eval_preprocessor(
+            tokenizer,
+            data_args,
+            max_seq_length,
+            pad_on_right,
+            eval_question_column_name,
+            eval_context_column_name,
+            eval_answer_column_name,
+        ),
+        batched=True,
+        num_proc=data_args.preprocessing_num_workers,
+        remove_columns=eval_column_names,
+        load_from_cache_file=not data_args.overwrite_cache,
+    )
+
+    return train_dataset, eval_dataset
